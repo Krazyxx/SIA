fcd0ebe4a814dde3f755300bb12e1c62725c86c6
Jimmy Gouraud <jimmy.gouraud@hotmail.fr>
2017-09-16 23:33:42 +0200

    TD1-3.1 - fix the display error of the shadow

--- TD1/src/whitted.cpp
+++ TD1/src/whitted.cpp
@@ -6,110 +6,111 @@
 class Whitted : public Integrator
 {
 public:
-    Whitted(const PropertyList &props) {
-        m_maxRecursion = props.getInteger("maxRecursion",4);
+  Whitted(const PropertyList &props) {
+    m_maxRecursion = props.getInteger("maxRecursion",4);
+  }
+
+  Color3f Li(const Scene *scene, const Ray &ray) const {
+
+    Color3f radiance = Color3f::Zero();
+
+    // stopping criteria:
+    if(ray.recursionLevel>=m_maxRecursion) {
+      return radiance;
     }
 
-    Color3f Li(const Scene *scene, const Ray &ray) const {
+    /* Find the surface that is visible in the requested direction */
+    Hit hit;
+    scene->intersect(ray, hit);
+    if (hit.foundIntersection())
+    {
+      Normal3f normal = hit.normal();
+      Point3f pos = ray.at(hit.t());
 
-        Color3f radiance = Color3f::Zero();
+      const Material* material = hit.shape()->material();
 
-        // stopping criteria:
-        if(ray.recursionLevel>=m_maxRecursion) {
-            return radiance;
+      const LightList &lights = scene->lightList();
+      for(LightList::const_iterator it=lights.begin(); it!=lights.end(); ++it)
+      {
+        Vector3f lightDir;
+        float dist;
+
+        if(dynamic_cast<const AreaLight*>(*it))
+        {
+          // source étendue
+          const AreaLight* light = dynamic_cast<const AreaLight*>(*it);
+
+          float uSize = Eigen::internal::random<float>(light->size()[0]/-2, light->size()[0]/2);
+          float vSize = Eigen::internal::random<float>(light->size()[1]/-2, light->size()[1]/2);
+          Point3f uPos = light->position() + uSize * light->uVec() + vSize * light->vVec();
+
+          lightDir = uPos - pos;
+          dist = lightDir.norm();
+          lightDir = lightDir.normalized();
+        } else {
+          // lampe ponctuelle ou directionnelle
+          lightDir = (*it)->direction(pos, &dist);
         }
 
-        /* Find the surface that is visible in the requested direction */
-        Hit hit;
-        scene->intersect(ray, hit);
-        if (hit.foundIntersection())
-        {
-            Normal3f normal = hit.normal();
-            Point3f pos = ray.at(hit.t());
+        Ray shadow_ray(pos + normal*Epsilon, lightDir, true);
+        Hit shadow_hit;
+        scene->intersect(shadow_ray,shadow_hit);
+        Color3f attenuation = Color3f(1.f);
+        if(shadow_hit.t()<dist){
+          if(!shadow_hit.shape()->isEmissive())
+          attenuation = 0.5f * shadow_hit.shape()->material()->transmissivness();
+          if((attenuation <= 1e-6).all())
+          continue;
+        }
 
-            const Material* material = hit.shape()->material();
+        float cos_term = std::max(0.f,lightDir.dot(normal));
+        Color3f brdf = material->brdf(-ray.direction, lightDir, normal, hit.texcoord());
+        radiance += (*it)->intensity(pos) * cos_term * brdf * attenuation;
+      }
 
-            const LightList &lights = scene->lightList();
-            for(LightList::const_iterator it=lights.begin(); it!=lights.end(); ++it)
-            {
-	      Vector3f lightDir;
-	      float dist;
+      // reflexions
+      if((material->reflectivity() > 1e-6).any())
+      {
+        Vector3f r = (ray.direction - 2.*ray.direction.dot(hit.normal())*hit.normal()).normalized();
+        Ray reflexion_ray(pos + hit.normal()*Epsilon, r);
+        reflexion_ray.recursionLevel = ray.recursionLevel + 1;
+        float cos_term = std::max(0.f,r.dot(normal));
+        radiance += material->reflectivity() * Li(scene, reflexion_ray) * cos_term;
+      }
 
-	      if(dynamic_cast<const AreaLight*>(*it))
-	      {
-		// source étendue
-		const AreaLight* light = dynamic_cast<const AreaLight*>(*it);
-		
-		float uSize = Eigen::internal::random<float>(0, light->size()[0]);
-		float vSize = Eigen::internal::random<float>(0, light->size()[1]);
-		Point3f uPos = light->position() + uSize * light->uVec() + vSize * light->vVec();
-		
-		lightDir = uPos - pos;
-                dist = lightDir.norm();
-		lightDir = lightDir.normalized();
-	      } else {
-		// lampe ponctuelle ou directionnelle
-                lightDir = (*it)->direction(pos, &dist);
-	      }
-	      
-	      Ray shadow_ray(pos + normal*Epsilon, lightDir, true);
-	      Hit shadow_hit;
-	      scene->intersect(shadow_ray,shadow_hit);
-	      Color3f attenuation = Color3f(1.f);
-	      if(shadow_hit.t()<dist){
-		if(!shadow_hit.shape()->isEmissive())
-		  attenuation = 0.5f * shadow_hit.shape()->material()->transmissivness();
-		if((attenuation <= 1e-6).all())
-		  continue;
-	      }
+      // refraction
+      if((material->transmissivness() > 1e-6).any())
+      {
+        float etaA = material->etaA(), etaB = material->etaB();
+        bool entering = -normal.dot(ray.direction) > 0;
+        if(!entering) {
+          std::swap(etaA,etaB);
+          normal = -normal;
+        }
+        Vector3f r;
+        if(refract(normal,ray.direction,etaA,etaB,r)) {
+          Ray refraction_ray(pos - normal*Epsilon, r);
+          refraction_ray.recursionLevel = ray.recursionLevel + 1;
+          float cos_term = std::max(0.f,-r.dot(normal));
+          radiance += material->transmissivness() * Li(scene, refraction_ray) * cos_term;
+        }
+      }
 
-	      float cos_term = std::max(0.f,lightDir.dot(normal));
-	      Color3f brdf = material->brdf(-ray.direction, lightDir, normal, hit.texcoord());
-	      radiance += (*it)->intensity(pos) * cos_term * brdf * attenuation;
-            }
-
-            // reflexions
-            if((material->reflectivity() > 1e-6).any())
-            {
-                Vector3f r = (ray.direction - 2.*ray.direction.dot(hit.normal())*hit.normal()).normalized();
-                Ray reflexion_ray(pos + hit.normal()*Epsilon, r);
-                reflexion_ray.recursionLevel = ray.recursionLevel + 1;
-                float cos_term = std::max(0.f,r.dot(normal));
-                radiance += material->reflectivity() * Li(scene, reflexion_ray) * cos_term;
-            }
-
-            // refraction
-            if((material->transmissivness() > 1e-6).any())
-            {
-                float etaA = material->etaA(), etaB = material->etaB();
-                bool entering = -normal.dot(ray.direction) > 0;
-                if(!entering) {
-                    std::swap(etaA,etaB);
-                    normal = -normal;
-                }
-                Vector3f r;
-                if(refract(normal,ray.direction,etaA,etaB,r)) {
-                    Ray refraction_ray(pos - normal*Epsilon, r);
-                    refraction_ray.recursionLevel = ray.recursionLevel + 1;
-                    float cos_term = std::max(0.f,-r.dot(normal));
-                    radiance += material->transmissivness() * Li(scene, refraction_ray) * cos_term;
-                }
-            }
-
-        }else if(ray.recursionLevel == 0)
-            return scene->backgroundColor();
-
-        return radiance;
+    } else if(ray.recursionLevel == 0) {
+      return scene->backgroundColor();
     }
 
-    std::string toString() const {
-        return tfm::format("Whitted[\n"
-                           "  max recursion = %f\n"
-                           " ]\n",
-                           m_maxRecursion);
-    }
+    return radiance;
+  }
+
+  std::string toString() const {
+    return tfm::format("Whitted[\n"
+    "  max recursion = %f\n"
+    " ]\n",
+    m_maxRecursion);
+  }
 private:
-    int m_maxRecursion;
+  int m_maxRecursion;
 };
 
 REGISTER_CLASS(Whitted, "whitted")
